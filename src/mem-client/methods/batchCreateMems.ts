d2feccf63700a6de0fb9bc7f04da1ced6e65e418
Scott Mackie <scott@mackie.io>
2022-03-21 13:51:28 -0700

    v0.1.1

--- src/mem-client/methods/batchCreateMems.ts
+++ src/mem-client/methods/batchCreateMems.ts
@@ -11,19 +11,24 @@ export const memClientBatchCreateMems =
    * Creates a new mem for each set of inputs, then returns the some details about the created mems.
    */
   async (batchArgs: MemClientBatchCreateMemsArgs) => {
-    const inputs = batchArgs.map(({ content, isRead, isArchived, scheduledFor, createdAt }) => {
-      const input = {
-        memId: null,
-        content,
-        format: null,
-        isRead: isRead ?? null,
-        isArchived: isArchived ?? null,
-        scheduledFor: scheduledFor ?? null,
-        createdAt: createdAt ?? null,
-      };
+    const inputs = batchArgs.map(
+      ({ memId, content, isRead, isArchived, scheduledFor, createdAt }) => {
+        const input = {
+          memId: memId ?? null,
+          content,
+          format: null,
+          isRead: isRead ?? null,
+          isArchived: isArchived ?? null,
+          scheduledFor: scheduledFor ?? null,
+          createdAt: createdAt ?? null,
+          richTextDocument: null,
+          source: null,
+          clientId: null,
+        };
 
-      return input;
-    });
+        return input;
+      }
+    );
 
     const result = await memClient.graphqlRequest(BatchCreateMemsDocument, {
       inputs,
--- src/utils/graphql/types.ts
+++ src/utils/graphql/types.ts
@@ -1,3 +1,19 @@
+/**
+ * @todo - We should update these types in the future to be more
+ * accurate (and remove "any".)
+ */
+
 export type UuidScalar = string;
 export type CursorScalar = string;
 export type DateTimeScalar = string;
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export type ColorScalar = any;
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export type AnyJsonObjectScalar = any;
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export type AnyJsonValueScalar = any;
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export type RichTextDocumentScalar = any;
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export type RichTextDeltaScalar = any;
+export type UploadScalar = unknown;
